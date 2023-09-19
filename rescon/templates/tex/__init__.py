c0028d43df3421c89786a26cd0cb472a6059ed1d
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-09-18 22:55:19 -0400

    Updates

--- rescon/templates/tex/__init__.py
+++ rescon/templates/tex/__init__.py
@@ -1,6 +1,32 @@
 import re
 from collections import defaultdict
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as MD
+
+def find_parent(root, child):
+    for parent in root.iter():
+        for cn in parent:
+            if child != cn:
+                continue
+            return parent
+    return None
+
+def handle_tag(tag_line, curr, q, data, root):
+    if "begin" in tag_line:
+        q.append(tag_line.split()[-1])
+        curr = ET.SubElement(curr, q[-1])
+        return curr, q, data, root
+    elif "end" in tag_line:
+        _ = q.pop()
+        if data:
+            curr.text = "\n".join(data["stored"])
+            data["stored"] = []
+        curr = find_parent(root, curr)
+        return curr, q, data, root
+    raise InvalidTagError()
+
+class InvalidTagError(Exception):
+    pass
 
 class TexTemplate:
     
@@ -9,33 +35,71 @@ class TexTemplate:
 
     def _convert_to_xml(self, lines):
         
-        xml = ET.Element("resume")
-        curr = xml
+        root = ET.Element("resume")
+        curr = root
         q = []
         i = 0
-        data = []
-
-        def handle_tag(tag_line):
-            if "begin" in tag_line:
-                q.append(tag_line.split()[-1])
-                curr = ET.SubElement(curr, q[-1])
-            elif "end" in tag_line:
-                _ = q.pop()
-                curr = curr.getparents()
+        data = {"en": False, "stored": []}
 
         while i < len(lines):
             line = lines[i].strip()
-            if not re.match("^{%.*%}$", line): continue
-            line = line[2:-2].strip()
-            if "tag" in line:
-
-
+            if re.match("^{%.*%}$", line):
+                line = line[2:-2].strip()
+                if "tag" in line:
+                    try:
+                        curr, q, data, root = handle_tag(line, curr, q, data, root)
+                    except InvalidTagError as e:
+                        print(ET.tostring(root))
+                elif "DATA" in line:
+                    data["en"] = not data["en"]
+                i += 1
+                continue
+            if data["en"]: data["stored"].append(line)
             i += 1
-        for i, line in lines:
-            line = line.strip()
+        
+        return root
 
+    def _update_tex_with_xml(self, lines):
+        
+        root = ET.Element("resume")
+        curr = root
+        q = []
+        i = 0
+        data = {"en": False, "stored": []}
 
+        while i < len(lines):
+            line = lines[i].strip()
+            if re.match("^{%.*%}$", line):
+                line = line[2:-2].strip()
+                if "tag" in line:
+                    try:
+                        curr, q, data, root = handle_tag(line, curr, q, data, root)
+                    except InvalidTagError as e:
+                        print(ET.tostring(root))
+                elif "DATA" in line:
+                    data["en"] = not data["en"]
+                i += 1
+                continue
+            if data["en"]: data["stored"].append(line)
+            i += 1
+        
+        return True
 
     def build(self):
         lines = self.file.readlines()
-        xml = self._convert_to_xml(lines)
+        return self._convert_to_xml(lines)
+
+    def modify(self, xml):
+        lines = self.file.readlines()
+        return self._update_tex_with_xml(lines)
+        
+        
+
+
+def main():
+    ttm = TexTemplate("/home/tanush/Programming/Projects/rescon/rescon/templates/tex/func.ttx")
+    root = ttm.build()
+    print(MD.parseString(ET.tostring(root).decode()).toprettyxml())
+
+if __name__ == "__main__":
+    main()
