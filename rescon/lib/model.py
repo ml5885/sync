6a2e5e02882700c368ab015bb9e3605764a578d0
Bluefin-Tuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2024-01-24 23:34:06 -0500

    feat: fixed display questions css and upgraded to gpt4-turbo

--- rescon/lib/model.py
+++ rescon/lib/model.py
@@ -1,4 +1,4 @@
 from langchain.chat_models import ChatOpenAI
 from ..config import server_config
 
-GPT = ChatOpenAI(openai_api_key=server_config.openai_api_key, model="gpt-3.5-turbo")
+GPT = ChatOpenAI(openai_api_key=server_config.openai_api_key, model="gpt-4-1106-preview", max_tokens=2048)
